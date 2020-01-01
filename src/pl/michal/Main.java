d3a15867c18c8399163b89744b0c9cd8b8f1c067
arhaniol <arhaniol@op.pl>
2020-01-01 14:30:34 +0100

    Optimizations and task2 according Samouczek way

--- src/pl/michal/Main.java
+++ src/pl/michal/Main.java
@@ -1,6 +1,8 @@
 package pl.michal;
 
 import java.util.*;
+import java.util.function.BinaryOperator;
+import java.util.stream.Stream;
 
 public class Main {
     static double highestRankingEver = 0;
@@ -18,16 +20,14 @@ public class Main {
     private static void task3() {
         Integer[] int1 = {1, 2, 3, 4, 5, 6};
         Integer[] int2 = {9, 8, 7, 6, 5, 4};
-        List<Integer> innerList1 = new ArrayList<>();
-        List<Integer> innerList2 = new ArrayList<>();
+        List<Integer> innerList1 = new ArrayList<>(Arrays.asList(int1));
+        List<Integer> innerList2 = new ArrayList<>(Arrays.asList(int2));
         List<List> outerList = new ArrayList<>();
-        innerList1.addAll(Arrays.asList(int1));
-        innerList2.addAll(Arrays.asList(int2));
         outerList.add(innerList1);
         outerList.add(innerList2);
 
         outerList.stream()
-                .flatMap(g -> g.stream())
+                .flatMap(Collection::stream)
                 .forEach(System.out::println);
     }
 
@@ -35,9 +35,15 @@ public class Main {
         Optional<BoardGame> game = BoardGame.GAMES.stream()
                 .reduce(Main::findLowest);
 
-        System.out.println(game.get().name);
+        Optional<BoardGame> minGame = min2(BoardGame.GAMES.stream(), Comparator.comparingDouble(g->g.rating));
+
+        System.out.println("My way: " +game.get().name);
+        System.out.println("Samouczek way: "+minGame.get().name);
     }
 
+    private static <T> Optional<T> min2(Stream<T> stream,Comparator<T> comparator){
+        return stream.reduce(BinaryOperator.minBy(comparator));
+    }
     private static BoardGame findLowest(BoardGame game, BoardGame game1) {
         if (game1.rating < game.rating)
             return game1;
